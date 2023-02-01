4eb7644794d4bea8633a347de6e2a65776c28932
Asmit2952 <asmitbm2952002@gmail.com>
2023-02-01 20:13:02 +0530

    work on loader

--- src/Components/Loader/Loader.jsx
+++ src/Components/Loader/Loader.jsx
@@ -5,7 +5,7 @@ import "./loader.css";
 export default function Loader() {
   return (
     <div className="flex justify-center items-center min-h-screen fixed top-0 left-0 w-full bg-offwhite loader-animate">
-      <img src={wow} alt="wow" className="w-[90%] sm:w-[auto]" />
+
     </div>
   );
 }
