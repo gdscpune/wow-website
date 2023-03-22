b30685b103adc568da5957c1b64bff129ec59ebe
Vinay Tiparadi <vinaytiparadi123@gmail.com>
2023-03-22 21:57:25 +0530

    rename waitlist button

--- src/Components/CTA-EarlyBird/CTA-speaker/ctaearlybird.jsx
+++ src/Components/CTA-EarlyBird/CTA-speaker/ctaearlybird.jsx
@@ -5,7 +5,7 @@ const CTAearlybird = () => {
     return (
         <div className="cta_speaker">
             <div className="cta_content">
-                <h3>Join the waitlist now!</h3>
+                <h3>Join the waitlist for Early Bird Ticket now!</h3>
             </div>
             <div className="cta_btn">
                 <button type="button"><a href="https://konfhub.com/checkout/gdscwowpune?ticketId=7558" target="_blank">Join Now!</a></button>
