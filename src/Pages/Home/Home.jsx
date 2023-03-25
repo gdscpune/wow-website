506266d0dae3d26ae2860e4048131add004625f0
Vinay Tiparadi <vinaytiparadi123@gmail.com>
2023-03-25 19:27:01 +0530

    update

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -71,7 +71,7 @@ export default function Home() {
           // </a>
           
           <span className="text-1xl lg:text-2xl ml-2 whitespace-nowrap">
-              🎟️Phase 2 | Early Bird Tickets Sold Out🎟️
+              🎟️Phase 2 | Early Bird Tickets are Sold Out!🎟️
             </span>
           
 
