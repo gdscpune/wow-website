8866865c253c0fc39a52d7253efeb0b7f2b2c2c3
Vinay Tiparadi <vinaytiparadi123@gmail.com>
2023-03-25 17:53:00 +0530

    update ticket, disable home animation for now

--- src/Components/CTA-EarlyBird/CTA-speaker/ctaearlybird.jsx
+++ src/Components/CTA-EarlyBird/CTA-speaker/ctaearlybird.jsx
@@ -5,10 +5,10 @@ const CTAearlybird = () => {
     return (
         <div className="cta_speaker">
             <div className="cta_content">
-                <h3>Join the waitlist for Early Bird Ticket now!</h3>
+                <h3>Phase 2 | Early Bird Tickets are live!</h3>
             </div>
             <div className="cta_btn">
-                <button type="button"><a href="https://konfhub.com/checkout/gdscwowpune?ticketId=7558" target="_blank">Join Now!</a></button>
+                <button type="button"><a href="https://konfhub.com/checkout/gdscwowpune?ticketId=7572" target="_blank">Buy Now!</a></button>
             </div>
         </div>
     )
