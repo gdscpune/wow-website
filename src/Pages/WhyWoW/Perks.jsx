aa66a9d2470cb0db620bb518ebd7520b11ec22b3
akash19here <spacex3nasa@gmail.com>
2023-03-29 16:42:01 +0530

    Added a json file for Card data and animated the card

--- src/Pages/WhyWoW/Perks.jsx
+++ src/Pages/WhyWoW/Perks.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import "./perks.css";
+import CardData from "./CardData.json";
 
 const Perks = () => {
   return (
@@ -43,58 +44,18 @@ const Perks = () => {
         </h3>
       </div>
       <div className="md:flex md:p-5 py-2 bg-slate-200 rounded-xl justify-space-between shadow-lg">
-        <div className="container flex justify-center p-0.5 my-4 mx-3 hover:scale-110">
-          <div className="content p-2 pb-8">
-            <p className="text-2xl text-center">💻Technical content</p>
-            <br />
-            <p className="mx-2">
-              GDSC WoW is offering you events hosted by the Google Developer
-              Groups community, featuring leading experts on Google technologies
-              in areas like Android, Firebase and so much more! So gear up and
-              get ready to enjoy the process of learning with us, because it's
-              Wonders of Wonders
-            </p>
-          </div>
+          {CardData.map((item) => {
+            return (
+              <div className="container flex justify-center p-0.5 my-4 mx-3 transform transition duration-500 hover:scale-110">
+                <div className="content p-2 pb-8">
+                  <p className="text-2xl text-center pt-2">{item.title}</p>
+                  <br />
+                  <p className="mx-2">{item.body}</p>
+                </div>
+              </div>
+            );
+          })}
         </div>
-        <div className="container flex justify-center p-0.5 my-4 mx-3 hover:scale-110">
-          <div className="content p-2 pb-8">
-            <p className="text-2xl text-center">🛠️Workshops & Codelabs</p>
-            <br />
-            <p className="mx-2">
-              Get ready to level up your skills and take your knowledge to the
-              next level with our exciting workshops and Google codelabs! GDSC
-              Wow offers a variety of hands-on learning experiences that will
-              help you master new technologies, tools and techniques.
-            </p>
-          </div>
-        </div>
-        <div className="container flex justify-center p-0.5 my-4 mx-3 hover:scale-110">
-          <div className="content p-2 pb-8">
-            <p className="text-2xl text-center">🌐Networking</p>
-            <br />
-            <p className="mx-2">
-              An event that big is going to open networking opportunities for
-              you without any doubt. GDSC Wow is designed to facilitate
-              networking. Whether you're looking to collaborate on a project,
-              find new ideas, discuss your ideas, or simply make new friends,
-              GDSC WoW is the perfect place to do it.
-            </p>
-          </div>
-        </div>
-        <div className="container flex justify-center p-0.5 my-4 mx-3 hover:scale-110">
-          <div className="content p-2 pb-8">
-            <p className="text-2xl text-center">🍕Swags & Refreshments</p>
-            <br />
-            <p className="mx-2">
-              Get ready for an unforgettable experience with swags and
-              refreshments that will leave you wanting more! From customized
-              goodies to event t-shirts, you'll receive the ultimate giveaway
-              items that will keep you connected to the event long after it's
-              over.
-            </p>
-          </div>
-        </div>
-      </div>
     </div>
   );
 };
