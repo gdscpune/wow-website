a3f146143eaaede35bcde7b42f18c828ee2a0ffb
Vinay Tiparadi <87959459+vinaytiparadi@users.noreply.github.com>
2023-03-30 18:58:35 +0530

    Update ctaearlybird.jsx

--- src/Components/CTA-EarlyBird/CTA-speaker/ctaearlybird.jsx
+++ src/Components/CTA-EarlyBird/CTA-speaker/ctaearlybird.jsx
@@ -5,7 +5,7 @@ const CTAearlybird = () => {
     return (
         <div className="cta_speaker">
             <div className="cta_content">
-                <h3>Get you General Access Ticket now!</h3>
+                <h3>Get your General Access Ticket now!</h3>
             </div>
             <div className="cta_btn">
                 <button type="button"><a href="https://konfhub.com/checkout/gdscwowpune" target="_blank">Buy Now!</a></button>
