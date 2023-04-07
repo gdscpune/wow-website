efdc4d98fe0f6e68f3c8c8a3c8d7718dc9effd44
Asmit2952 <asmitbm2952002@gmail.com>
2023-04-07 21:15:34 +0530

    updates in community partners section

--- src/Pages/Community_Partners_New/CommunityCard.jsx
+++ src/Pages/Community_Partners_New/CommunityCard.jsx
@@ -6,7 +6,7 @@ export default function CommunityCard({ item }) {
       <a
         href={item.url}
         target="_blank"
-        className="p-0 lg:p-4 w-[70%] bg-slate-200 lg:w-1/4 h-[100px] lg:h-[200px] flex items-center flex-row flex-wrap lg:flex-row justify-evenly rounded-xl"
+        className="p-0 lg:p-4 w-[70%] bg-white shadow-lg lg:w-1/4 h-[100px] lg:h-[200px] flex items-center flex-row flex-wrap lg:flex-row justify-evenly rounded-xl"
       >
         <img
           src={item.logo}
