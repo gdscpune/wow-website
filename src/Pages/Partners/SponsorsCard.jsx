853feefc2a8711d19161e891fb7b4be2bff4cb88
Vinay Tiparadi <vinaytiparadi123@gmail.com>
2023-04-10 10:04:33 +0530

    update sponsorcard

--- src/Pages/Partners/SponsorsCard.jsx
+++ src/Pages/Partners/SponsorsCard.jsx
@@ -6,14 +6,14 @@ export default function SponsorsCard({ item }) {
       <a
         href={item.url}
         target='_blank'
-        className='p-4 w-[90%] bg-primary text-white lg:w-2/5 h-[300px] flex items-center flex-row flex-wrap lg:flex-row justify-evenly rounded-xl'
+        className='p-0 lg:p-4 w-[70%] bg-white shadow-lg lg:w-1/4 h-[100px] lg:h-[200px] flex items-center flex-row flex-wrap lg:flex-row justify-evenly rounded-xl'
       >
         <img
           src={item.logo}
           alt=''
-          className='w-2/5 lg:w-2/5 rounded bg-transparent mr-2'
+          className='w-2/5 lg:w-4/5 rounded bg-transparent mr-2'
         />
-        <h1 className='mt-3 text-3xl'>{item.name}</h1>
+        {/* <h1 className='mt-3 text-3xl'>{item.name}</h1> */}
       </a>
     </>
   );
