71521eca25061d25d30e2aac062f90f2890deaeb
Vinay Tiparadi <vinaytiparadi123@gmail.com>
2023-04-13 17:20:35 +0530

    wow speakers final update

--- src/assets/img/speaker/index.js
+++ src/assets/img/speaker/index.js
@@ -1,12 +1,20 @@
-import VrijrajSingh from "./VrijrajSingh.jpg";
-import ShreeBhagwat from "./ShreeBhagwat.png";
-import PoojaBhaumik from "./PoojaBhaumik.jpg";
-import NikitaGandhi from "./NikitaGandhi.jpeg";
+import VrijrajSingh from "./VrijrajSingh.png";
+import ShreeBhagwat from "./ShrihridayBhagwat.png";
+import PoojaBhaumik from "./PoojaBhaumik.png";
+import NikitaGandhi from "./NikitaGandhi.png";
 import YogeshKulkarni from "./YogeshKulkarni.png";
-import MurariRamuka from "./MurariRamuka.jpg"
-import SoumyaMalgode from "./SoumyaMalgode.jpeg";
+import MurariRamuka from "./MurariRamuka.png"
+import SoumyaMalgode from "./SoumyaMalgonde.png";
+import KartikPadmanabhan from "./KartikPadmanabhan.png";
+import SangeetaGupta from "./SangeetaGupta.png";
+import UdayenduKar from "./UdayenduKar.png";
+import FaizanAkhtar from "./FaizanAkhtar.png";
 
 export {
+  FaizanAkhtar,
+  KartikPadmanabhan,
+  UdayenduKar,
+  SangeetaGupta,
   VrijrajSingh,
   ShreeBhagwat,
   PoojaBhaumik,
